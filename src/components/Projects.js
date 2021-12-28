40c7864d3403c354482ad0f08dd64ca4eaeeacce
Alex George <alexgeorge2863@gmail.com>
2021-12-28 11:08:06 -0700

    add perojects section with peoject cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,82 @@
 import React from 'react'
+import cardImg from '../images/four.jpg'
 
 export default function Project() {
 	return (
-		<section className='projects-section'>
-			<h1 className='project-title'>Here are some of my personal projects</h1>
+		<section class='project-section active'>
+			<h1 class='project-heading'>some of my projects</h1>
+			
+			<div class='project-container'>
+				<div class='project-card'>
+					<img src={cardImg} class='project-img' alt='' />
+					<div class='project-content'>
+						<h1 class='project-title'>project 01</h1>
+						<p class='project-info'>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+						</p>
+						<div class='project-btn-grp'>
+							<button class='project-btn github'>github repo</button>
+							<button class='project-btn live'>see live</button>
+						</div>
+					</div>
+				</div>
+
+				<div class='project-card'>
+					<img src={cardImg} class='project-img' alt='' />
+					<div class='project-content'>
+						<h1 class='project-title'>project 01</h1>
+						<p class='project-info'>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+						</p>
+						<div class='project-btn-grp'>
+							<button class='project-btn github'>github repo</button>
+							<button class='project-btn live'>see live</button>
+						</div>
+					</div>
+				</div>
+
+				<div class='project-card'>
+					<img src={cardImg} class='project-img' alt='' />
+					<div class='project-content'>
+						<h1 class='project-title'>project 01</h1>
+						<p class='project-info'>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+						</p>
+						<div class='project-btn-grp'>
+							<button class='project-btn github'>github repo</button>
+							<button class='project-btn live'>see live</button>
+						</div>
+					</div>
+				</div>
+
+				<div class='project-card'>
+					<img src={cardImg} class='project-img' alt='' />
+					<div class='project-content'>
+						<h1 class='project-title'>project 01</h1>
+						<p class='project-info'>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+						</p>
+						<div class='project-btn-grp'>
+							<button class='project-btn github'>github repo</button>
+							<button class='project-btn live'>see live</button>
+						</div>
+					</div>
+				</div>
+
+				<div class='project-card'>
+					<img src={cardImg} class='project-img' alt='' />
+					<div class='project-content'>
+						<h1 class='project-title'>project 01</h1>
+						<p class='project-info'>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+						</p>
+						<div class='project-btn-grp'>
+							<button class='project-btn github'>github repo</button>
+							<button class='project-btn live'>see live</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	)
 }
